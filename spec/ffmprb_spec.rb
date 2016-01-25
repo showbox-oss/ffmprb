b63be50e8457fecd300140aa60826b5cae258f2b
Fedot Praslov <fedot@praslov.com>
2016-01-25 13:41:49 +0200

    corrected specs (audio ducking)

--- spec/ffmprb_spec.rb
+++ spec/ffmprb_spec.rb
@@ -638,7 +638,7 @@ describe Ffmprb do
           expect(wave_data(sound).frequency).to be_within(10).of NOTES.G6
         end
 
-        expect(@av_out_stream.length).to be_approximately 10
+        expect(@av_out_stream.length).to be_approximately 16
       end
 
       it "should duck some overlay sound wrt some main sound" do
